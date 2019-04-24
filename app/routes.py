c058daeaa1f6bf03be3bd60ece31b2af80d23d37
deanvains <dean.vains@outlook.com>
2019-04-24 14:14:12 +0800

    Changes to routes.py to include new paths'

--- app/routes.py
+++ app/routes.py
@@ -12,45 +12,40 @@ from app.forms import RegistrationForm
 
 
 @app.route('/')
-@app.route('/index')
+@app.route('/homepage')
 @login_required
-def index():
+def homepage():
 
     posts = [
         {
-            'author': {'username': 'Harry'},
-            'body': 'Beautiful day in Coolbellup!'
+            'author': {'username': 'Example'},
+            'body': 'Example!'
         },
         {
-            'author': {'username': 'Caleb'},
-            'body': 'The Avengers movie was so cool!'
+            'author': {'username': 'Example'},
+            'body': 'Example'
         }
     ]
-    return render_template('index.html', title='Home', posts=posts)
+    return render_template('homepage.html', title='Home', posts=posts)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
+@app.route('/signin', methods=['GET', 'POST'])
+def signin():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('homepage'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
-            return redirect(url_for('login'))
+            return redirect(url_for('signin'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
+            next_page = url_for('homepage')
         return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
+    return render_template('signin.html', title='Sign In', form=form)
 
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-@app.route('/register', methods=['GET', 'POST'])
+@app.route('/signin', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
@@ -63,3 +58,24 @@ def register():
         flash('Congratulations, you are now a registered user!')
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('homepage'))
+
+@app.route('/tutorialpage')
+def tutorial():
+    return redirect(url_for('tutorialpage'))
+
+@app.route('/topicpage')
+def topic():
+     return redirect(url_for('topicpage'))
+
+@app.route('/profile')
+def profile():
+    return redirect(url_for('profile'))
+
+@app.route('/contact')
+def contact():
+    return redirect(url_for('contact'))
