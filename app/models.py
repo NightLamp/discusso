a08be1c17f45c4d553330a8a762447346379bc43
nightlamp <benjaminlongbottom98@gmail.com>
2019-04-25 13:57:22 +0800

    added database models User, Post & Reply. Reply is not self relational

--- app/models.py
+++ app/models.py
@@ -8,6 +8,62 @@ from flask_login import UserMixin
 def load_user(id):
     return User.query.get(int(id))
 
+
+class User(UserMixin, db.Model):
+    id            = db.Column(db.Integer, primary_key=True)
+    username      = db.Column(db.String(64), index=True, unique=True)
+    bio           = db.Column(db.String(248))
+    passwd_hash   = db.Column(db.String(128))
+    email         = db.Column(db.String(120), index=True, unique=True)
+    first_name    = db.Column(db.String(20))
+    last_name     = db.Column(db.String(20))
+    # date_of_birth = db.Column(db.
+
+    posts         = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<id{}, User {}>'.format(self.id, self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Post(db.Model):
+    id        = db.Column(db.Integer, primary_key=True)
+    title     = db.Column(db.String(64))
+    desc      = db.Column(db.String(248))
+    blesses   = db.Column(db.Integer)
+    curses    = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id   = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    p_replies   = db.relationship('Reply', backref='ogPost', lazy='dynamic')
+
+    def __repr__(self):
+        return '<id {}, Post {}>'.format(self.id, self.title) 
+
+class Reply(db.Model):
+    id        = db.Column(db.Integer, primary_key=True)
+    post_id   = db.Column(db.Integer, db.ForeignKey('post.id'))
+#     reply_id  = db.Column(db.Integer, db.ForeignKey('reply.id'))
+    text      = db.Column(db.String(248))
+    blesses   = db.Column(db.Integer)
+    curses    = db.Column(db.Integer)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    stance    = db.Column(db.Boolean)
+     
+    # r_replies   = db.relationship('Reply', backref='ogPost', lazy='dynamic')
+ 		
+    def __repr__(self):
+        return '<id {}, Reply {}>'.format(self.id, self.text)
+
+
+
+# Old code from mega tutorial
+'''
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -32,6 +88,6 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
-
+'''
 
 
