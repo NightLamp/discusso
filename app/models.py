f474750029a8863bf76b2d52103ed0bde2a8d34f
deanvains <dean.vains@outlook.com>
2019-05-10 15:32:31 +0800

    test db running and other stuff

--- app/models.py
+++ app/models.py
@@ -15,6 +15,7 @@ class User(UserMixin, db.Model):
     bio           = db.Column(db.String(248))
     passwd_hash   = db.Column(db.String(128))
     email         = db.Column(db.String(120), index=True, unique=True)
+    admin         = db.Column(db.Boolean)
     
     posts         = db.relationship('Post', backref='author', lazy='dynamic')
 
