b96fb110baf01d040e82e1da035f0ef0214fb5ca
nightlamp <benjaminlongbottom98@gmail.com>
2019-05-19 18:58:16 +0800

    added Reply_BC to shell

--- discusso.py
+++ discusso.py
@@ -1,7 +1,7 @@
 from app import app, db
-from app.models import User, Post, Reply, Post_BC
+from app.models import User, Post, Reply, Post_BC, Reply_BC
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Post': Post, 'Reply': Reply, 'Post_BC':Post_BC}
+    return {'db': db, 'User': User, 'Post': Post, 'Reply': Reply, 'Post_BC':Post_BC, 'Reply_BC':Reply_BC}
 
